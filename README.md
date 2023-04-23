174fa16bcabd73a7bdff6d94913ccb5b13e3aee7
ilonadem <alenkademler@gmail.com>
2023-04-23 13:09:08 -0400

    initial commit (cleaned python-posenet repo)

---
+++ README.md
@@ -0,0 +1,68 @@
+## Testing Pose Models on MCL Data
+
+Based on PoseNet infrastructure of this repository is based on this repo: (https://github.com/rwightman/posenet-python)
+
+### Install
+
+To install the required packages in a conda environment:
+
+```
+conda env create -f environment.yml
+conda activate tf2
+```
+
+A suitable Python 3.x environment with a recent version of Tensorflow is required.
+
+This relies on an older version of tensorflow, so it requires disabling tensorflow v2 behavior in any scripts:
+```
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+``` 
+
+<!-- Development and testing was done with Conda Python 3.6.8 and Tensorflow 1.12.0 on Linux.
+
+Windows 10 with the latest (as of 2019-01-19) 64-bit Python 3.7 Anaconda installer was also tested. -->
+
+If you want to use the webcam demo, a pip version of opencv (`pip install opencv-python`) is required instead of the conda version. Anaconda's default opencv does not include ffpmeg/VideoCapture support. Also, you may have to force install version 3.4.x as 4.x has a broken drawKeypoints binding.
+
+A conda environment setup as below should suffice: 
+```
+conda install tensorflow-gpu scipy pyyaml python=3.6
+pip install opencv-python==3.4.5.20
+
+```
+
+### Usage
+
+There are three demo apps in the root that utilize the PoseNet model. They are very basic and could definitely be improved.
+
+The first time these apps are run (or the library is used) model weights will be downloaded from the TensorFlow.js version and converted on the fly.
+
+For all demos, the model can be specified with the '--model` argument by using its ordinal id (0-3) or integer depth multiplier (50, 75, 100, 101). The default is the 101 model.
+
+#### image_demo.py 
+
+Image demo runs inference on an input folder of images and outputs those images with the keypoints and skeleton overlayed.
+
+`python image_demo.py --model 101 --image_dir ./images --output_dir ./output`
+
+A folder of suitable test images can be downloaded by first running the `get_test_images.py` script.
+
+#### benchmark.py
+
+A minimal performance benchmark based on image_demo. Images in `--image_dir` are pre-loaded and inference is run `--num_images` times with no drawing and no text output.
+
+#### webcam_demo.py
+
+The webcam demo uses OpenCV to capture images from a connected webcam. The result is overlayed with the keypoints and skeletons and rendered to the screen. The default args for the webcam_demo assume device_id=0 for the camera and that 1280x720 resolution is possible.
+
+### Credits
+
+The PoseNet and model infra structure of this repository is based on this repo: (https://github.com/rwightman/posenet-python)
+
+The original model, weights, code, etc. was created by Google and can be found at https://github.com/tensorflow/tfjs-models/tree/master/posenet
+
+
+
+
+
---
+++ webcam_demo.py
@@ -0,0 +1,106 @@
+# import tensorflow as tf
+import cv2
+import time
+import argparse
+
+import posenet
+
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', type=int, default=101)
+parser.add_argument('--cam_id', type=int, default=0)
+parser.add_argument('--cam_width', type=int, default=1280)
+parser.add_argument('--cam_height', type=int, default=720)
+parser.add_argument('--scale_factor', type=float, default=0.7125)
+parser.add_argument('--file', type=str, default=None, help="Optionally use a video file instead of a live camera")
+args = parser.parse_args()
+
+
+def main():
+    with tf.Session() as sess:
+        model_cfg, model_outputs = posenet.load_model(args.model, sess)
+        output_stride = model_cfg['output_stride']
+
+        if args.file is not None:
+            cap = cv2.VideoCapture(args.file)
+        else:
+            cap = cv2.VideoCapture(args.cam_id)
+        cap.set(3, args.cam_width)
+        cap.set(4, args.cam_height)
+
+        start = time.time()
+        frame_count = 0
+        while True:
+            input_image, display_image, output_scale = posenet.read_cap(
+                cap, scale_factor=args.scale_factor, output_stride=output_stride)
+
+            heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = sess.run(
+                model_outputs,
+                feed_dict={'image:0': input_image}
+            )
+
+            pose_scores, keypoint_scores, keypoint_coords = posenet.decode_multi.decode_multiple_poses(
+                heatmaps_result.squeeze(axis=0),
+                offsets_result.squeeze(axis=0),
+                displacement_fwd_result.squeeze(axis=0),
+                displacement_bwd_result.squeeze(axis=0),
+                output_stride=output_stride,
+                max_pose_detections=10,
+                min_pose_score=0.15)
+
+            keypoint_coords *= output_scale
+
+            # TODO this isn't particularly fast, use GL for drawing and display someday...
+            overlay_image = posenet.draw_skel_and_kp(
+                display_image, pose_scores, keypoint_scores, keypoint_coords,
+                min_pose_score=0.15, min_part_score=0.1)
+
+            cv2.imshow('posenet', overlay_image)
+            frame_count += 1
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+
+        print('Average FPS: ', frame_count / (time.time() - start))
+
+
+if __name__ == "__main__":
+    main()
+
+
+###############
+
+import cv2
+import numpy as np
+ 
+# Create a VideoCapture object and read from input file
+# If the input is the camera, pass 0 instead of the video file name
+cap = cv2.VideoCapture('videos/ilona-cam-test.mp4')
+ 
+# Check if camera opened successfully
+if (cap.isOpened()== False): 
+  print("Error opening video stream or file")
+ 
+# Read until video is completed
+while(cap.isOpened()):
+  # Capture frame-by-frame
+  ret, frame = cap.read()
+  if ret == True:
+ 
+    # Display the resulting frame
+    cv2.imshow('Frame',frame)
+ 
+    # Press Q on keyboard to  exit
+    if cv2.waitKey(25) & 0xFF == ord('q'):
+      break
+ 
+  # Break the loop
+  else: 
+    break
+ 
+# When everything done, release the video capture object
+cap.release()
+ 
+# Closes all the frames
+cv2.destroyAllWindows()
